a9cc8053a395a8871883b49c5b594f2494d4a519
Lebo-Kharafu <Kharafu23@gmail.com>
2026-02-07 23:13:04 +0200

    feat: made layout

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,110 @@
 <template>
-  <main id="page">
+  <section id="page">
     <section id="nav">
       <Navbar :links="[
-        { icon: '../../public/vite.svg', alt: 'image' },
-        { icon: '../../public/vite.svg', alt: 'image' },
-        { icon: '../../public/vite.svg', alt: 'image' }
-      ]" />
+        { icon: 'https://img.icons8.com/?size=65&id=13930&format=png&color=000000', alt: 'image' },
+        { icon: 'https://img.icons8.com/?size=65&id=32323&format=png&color=000000', alt: 'image' },
+        { icon: 'https://img.icons8.com/?size=65&id=118640&format=png&color=000000', alt: 'image' },
+        { icon: 'https://img.icons8.com/?size=65&id=63777&format=png&color=000000', alt: 'image' },
+        { icon: 'https://img.icons8.com/?size=65&id=34886&format=png&color=000000', alt: 'image' },
+        { icon: 'https://img.icons8.com/?size=100&id=aE4hnRenCYJB&format=png&color=000000', alt: 'image' },
+      ]" :avatar="{ icon: './vite.svg', alt: 'image' }" 
+      />
     </section>
     <section id="mid">
-      <LayoutContainer />
+      <LayoutContainer image="../src/assets/inspo/content.jpg" height="100%" width="100%" main-text-color="#fa803a"
+        sub-color="#fa803a">
+
+        <template #main>
+          {{ }}
+        </template>
+        <template #sub>
+          <h1> Bigger Sub stuff</h1>
+        </template>
+
+      </LayoutContainer>
     </section>
     <section id="side">
-      <StackCards size="150px" />
+      <StackCards width="100%" height="210px">
+        <template #card-1></template>
+        <template #card-2></template>
+        <template #card-3></template>
+      </StackCards>
+      <SubBox>
+        <template #content>
+          {{ "Hello" }}
+        </template>
+      </SubBox>
     </section>
-  </main>
+  </section>
 </template>
 
 <script setup lang="ts">
   import LayoutContainer from "./components/LayoutContainer.vue";
   import Navbar from "./components/Navbar.vue";
   import StackCards from "./components/StackCards.vue";
+import SubBox from "./components/SubBox.vue";
 </script>
 
 
 <style scoped>
+  #nav {
+    grid-area: nav;
+    max-width: 60px;
+  }
+
+  #mid {
+    grid-area: mid;
+  }
+
   #side {
-    min-width: 200px;
+    grid-area: side;
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
   }
 
   #page {
-    display: flex;
-    flex-direction: row;
-    gap: 1rem;
+    max-width: 100%;
+    min-height: 100%;
+    padding: 0.5rem;
+    display: grid;
+    grid-template-columns: auto 2fr 1fr;
+    column-gap: 1rem;
+    grid-template-areas:
+      "nav mid side"
+    ;
   }
+
+  @media (max-width: 780px) {
+    #page {
+      display: grid;
+      grid-template-rows: auto 2fr 1fr;
+      grid-template-columns: 1fr;
+      row-gap: 1rem;
+      grid-template-areas:
+        "nav"
+        "mid"
+        "side"
+      ;
+    }
+
+    #nav{
+      max-width: none;
+    }
+
+    #side {
+      display: flex;
+      padding: 0rem 0.5rem;
+      flex-direction: row-reverse;
+      gap: 1.5rem;
+    }
+
+  }
+
+   @media (max-width: 430px) {
+     #side {
+      flex-direction: column;
+    }
+   }
 </style>
